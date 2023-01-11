7748731c488fffcf376a46f323b4c3786cf41a02
Santiago Urquia <santiurquia7@gmail.com>
2023-01-11 16:10:11 -0300

    MaquetadoANGULAR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { EstudiosComponent } from './estudios/estudios.component';
+import { SkillComponent } from './skill/skill.component';
+import { WorksComponent } from './works/works.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    AboutmeComponent,
+    EstudiosComponent,
+    SkillComponent,
+    WorksComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
